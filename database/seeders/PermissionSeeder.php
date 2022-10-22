ee6ec56e79803636e8c03456e658f625f61c8ec4
Alexander Arregui <mosketxu@gmail.com>
2022-10-22 21:12:43 +0200

    seeder

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -23,52 +23,59 @@ class PermissionSeeder extends Seeder
         // \\DB::table('permissions')->delete();
 
         // Users
-        Permission::create(['name'=>'user.index'])->syncRoles($admin, $gestor);
-        Permission::create(['name'=>'user.create'])->syncRoles($admin, $gestor);
-        Permission::create(['name'=>'user.edit'])->syncRoles($admin, $gestor);
-        Permission::create(['name'=>'user.update'])->syncRoles($admin, $gestor);
-        Permission::create(['name'=>'user.delete'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'user.index'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'user.create'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'user.edit'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'user.update'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'user.delete'])->syncRoles($admin, $gestor);
 
         // Roles
-        Permission::create(['name'=>'role.index'])->syncRoles($admin, $gestor);
-        Permission::create(['name'=>'role.create'])->syncRoles($admin, $gestor);
-        Permission::create(['name'=>'role.edit'])->syncRoles($admin, $gestor);
-        Permission::create(['name'=>'role.update'])->syncRoles($admin, $gestor);
-        Permission::create(['name'=>'role.delete'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'role.index'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'role.create'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'role.edit'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'role.update'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'role.delete'])->syncRoles($admin, $gestor);
 
         // Permisos
-        Permission::create(['name'=>'permiso.index'])->syncRoles($admin, $gestor);
-        Permission::create(['name'=>'permiso.create'])->syncRoles($admin, $gestor);
-        Permission::create(['name'=>'permiso.edit'])->syncRoles($admin, $gestor);
-        Permission::create(['name'=>'permiso.update'])->syncRoles($admin, $gestor);
-        Permission::create(['name'=>'permiso.delete'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'permiso.index'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'permiso.create'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'permiso.edit'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'permiso.update'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'permiso.delete'])->syncRoles($admin, $gestor);
 
         // Seguridad
-        Permission::create(['name'=>'seguridad.index'])->syncRoles($admin, $gestor);
-        Permission::create(['name'=>'seguridad.create'])->syncRoles($admin, $gestor);
-        Permission::create(['name'=>'seguridad.edit'])->syncRoles($admin, $gestor);
-        Permission::create(['name'=>'seguridad.update'])->syncRoles($admin, $gestor);
-        Permission::create(['name'=>'seguridad.delete'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'seguridad.index'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'seguridad.create'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'seguridad.edit'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'seguridad.update'])->syncRoles($admin, $gestor);
+        // Permission::create(['name'=>'seguridad.delete'])->syncRoles($admin, $gestor);
 
         // Entidades
-        Permission::create(['name'=>'entidad.index'])->syncRoles($admin, $gestor, $milimetrica);
-        Permission::create(['name'=>'entidad.create'])->syncRoles($admin, $gestor, $milimetrica);
-        Permission::create(['name'=>'entidad.edit'])->syncRoles($admin, $gestor, $milimetrica);
-        Permission::create(['name'=>'entidad.update'])->syncRoles($admin, $gestor, $milimetrica);
-        Permission::create(['name'=>'entidad.delete'])->syncRoles($admin, $gestor, $milimetrica);
+        // Permission::create(['name'=>'entidad.index'])->syncRoles($admin, $gestor, $milimetrica);
+        // Permission::create(['name'=>'entidad.create'])->syncRoles($admin, $gestor, $milimetrica);
+        // Permission::create(['name'=>'entidad.edit'])->syncRoles($admin, $gestor, $milimetrica);
+        // Permission::create(['name'=>'entidad.update'])->syncRoles($admin, $gestor, $milimetrica);
+        // Permission::create(['name'=>'entidad.delete'])->syncRoles($admin, $gestor, $milimetrica);
 
         // Entidades contactos
-        Permission::create(['name'=>'entidadcontacto.index'])->syncRoles($admin, $gestor, $milimetrica);
-        Permission::create(['name'=>'entidadcontacto.create'])->syncRoles($admin, $gestor, $milimetrica);
-        Permission::create(['name'=>'entidadcontacto.edit'])->syncRoles($admin, $gestor, $milimetrica);
-        Permission::create(['name'=>'entidadcontacto.update'])->syncRoles($admin, $gestor, $milimetrica);
-        Permission::create(['name'=>'entidadcontacto.delete'])->syncRoles($admin, $gestor, $milimetrica);
+        // Permission::create(['name'=>'entidadcontacto.index'])->syncRoles($admin, $gestor, $milimetrica);
+        // Permission::create(['name'=>'entidadcontacto.create'])->syncRoles($admin, $gestor, $milimetrica);
+        // Permission::create(['name'=>'entidadcontacto.edit'])->syncRoles($admin, $gestor, $milimetrica);
+        // Permission::create(['name'=>'entidadcontacto.update'])->syncRoles($admin, $gestor, $milimetrica);
+        // Permission::create(['name'=>'entidadcontacto.delete'])->syncRoles($admin, $gestor, $milimetrica);
 
         // Productos
-        Permission::create(['name'=>'producto.index'])->syncRoles($admin, $gestor, $milimetrica);
-        Permission::create(['name'=>'producto.create'])->syncRoles($admin, $gestor, $milimetrica);
-        Permission::create(['name'=>'producto.edit'])->syncRoles($admin, $gestor, $milimetrica);
-        Permission::create(['name'=>'producto.update'])->syncRoles($admin, $gestor, $milimetrica);
-        Permission::create(['name'=>'producto.delete'])->syncRoles($admin, $gestor, $milimetrica);
+        // Permission::create(['name'=>'producto.index'])->syncRoles($admin, $gestor, $milimetrica);
+        // Permission::create(['name'=>'producto.create'])->syncRoles($admin, $gestor, $milimetrica);
+        // Permission::create(['name'=>'producto.edit'])->syncRoles($admin, $gestor, $milimetrica);
+        // Permission::create(['name'=>'producto.update'])->syncRoles($admin, $gestor, $milimetrica);
+        // Permission::create(['name'=>'producto.delete'])->syncRoles($admin, $gestor, $milimetrica);
+
+        // pedidos
+        Permission::create(['name'=>'pedido.index'])->syncRoles($admin, $gestor, $milimetrica);
+        Permission::create(['name'=>'pedido.create'])->syncRoles($admin, $gestor, $milimetrica);
+        Permission::create(['name'=>'pedido.edit'])->syncRoles($admin, $gestor, $milimetrica);
+        Permission::create(['name'=>'pedido.update'])->syncRoles($admin, $gestor, $milimetrica);
+        Permission::create(['name'=>'pedido.delete'])->syncRoles($admin, $gestor, $milimetrica);
     }
 }
