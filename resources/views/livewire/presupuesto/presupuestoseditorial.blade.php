b56deb865c8dc4342608f7baecff7986df135f1a
Alexander Arregui <mosketxu@gmail.com>
2023-02-25 17:43:36 +0100

    vista presupuestos

--- resources/views/livewire/presupuesto/presupuestoseditorial.blade.php
+++ resources/views/livewire/presupuesto/presupuestoseditorial.blade.php
@@ -12,51 +12,33 @@
                 <div>
                     <div class="">
                         <div class="flex w-full pt-2 pb-0 space-x-1 text-sm font-bold text-gray-500 bg-blue-100 rounded-t-md ">
-                            <div class="flex w-5 h-5 px-2 mr-2 font-medium text-center" >
-                                <x-input.checkbox wire:model="selectPage" />
-                            </div>
                             <div class="w-1/12 text-left" >{{ __('Nº.Presup') }}</div>
                             <div class="w-1/12 text-left" >{{ __('Factura') }}</div>
                             <div class="w-2/12 text-left" >{{ __('Cliente') }}</div>
-                            <div class="w-2/12 text-left" >{{ __('ISBN') }} </div>
-                            <div class="w-2/12 text-left" >{{ __('Referencia') }}</div>
+                            <div class="w-2/12 text-left" >{{ __('ISBN / Referencia') }} </div>
                             <div class="w-2/12 text-left">{{ __('F.Presup') }}</div>
                             {{-- <div class="w-1/12 text-left md:" >{{ __('Proveedor') }}</div> --}}
                             {{-- <div class="w-1/12 text-right">{{ __('Tirada') }}</div> --}}
                             {{-- <div class="w-1/12 text-right">{{ __('Precio Ud.') }}</div> --}}
                             {{-- <div class="w-1/12 text-right">{{ __('Precio Total') }}</div> --}}
-                            <div class="w-2/12 text-center">{{ __('Estado') }}</div>
+                            <div class="w-1/12 text-center">{{ __('Estado') }}</div>
                             <div class="w-1/12 text-center">{{ __('Pedido') }}</div>
-                            <div class="w-1/12 text-left" ></div>
+                            <div class="w-2/12 text-left" ></div>
                         </div>
                     </div>
                     <div>
-                        @if($selectPage)
-                        <div class="flex w-full text-sm text-left bg-gray-200 border-t-0 border-y" wire:key="row-message">
-                            <div class="flex-col w-full text-left">
-                                @unless($selectAll)
-                                    <span>Has seleccionado <strong>{{ $presupuesto->count() }}</strong> presupuestos, ¿quieres seleccionar el total: <strong>{{ $presupuesto->total() }}</strong> ?</span>
-                                    <x-button.link wire:click="selectAll" class="ml-1 text-blue-600">Select all</x-button.link>
-                                @else
-                                    <span>Has seleccionado <strong>todos</strong> los {{ $presupuesto->total() }} presupuestos</span>
-                                @endif
-                            </div>
-                        </div>
-                        @endif
                         @forelse ($presupuestos as $presupuesto)
                         <div class="flex w-full space-x-1 text-sm text-gray-500 border-t-0 border-y hover:bg-gray-100" wire:loading.class.delay="opacity-50">
-                            <div class="flex w-5 h-5 p-2 mr-2 font-medium text-center"><x-input.checkbox wire:model="selected" value="{{ $presupuesto->id }}" /></div>
                             <div class="flex-col w-1/12 my-2 text-left">{{ $presupuesto->id }}</div>
                             <div class="flex-col w-1/12 my-2 text-left">{{ $presupuesto->facturadopor=='1' ? 'MM' : 'Proveedor' }}</div>
                             <div class="flex-col w-2/12 my-2 text-left">{{ $presupuesto->cliente->entidad }}</div>
-                            <div class="flex-col w-2/12 my-2 text-left">{{ $presupuesto->isbn }}</div>
-                            <div class="flex-col w-2/12 my-2 text-left">{{ $presupuesto->referencia }}</div>
+                            <div class="flex-col w-2/12 my-2 text-left">{{ $presupuesto->isbn }} {{ $presupuesto->referencia }}</div>
                             <div class="flex-col w-2/12 my-2 text-left">{{ $presupuesto->fpresupuesto4 }}</div>
                             {{-- <div class="flex-col w-1/12 my-2 text-left">{{ $presupuesto->proveedor->entidad }}</div> --}}
                             {{-- <div class="flex-col w-1/12 my-2 text-right">{{ $presupuesto->tirada }}</div> --}}
                             {{-- <div class="flex-col w-1/12 my-2 text-right">{{ $presupuesto->precio_ud }}</div> --}}
                             {{-- <div class="flex-col w-1/12 my-2 text-right">{{ $presupuesto->preciototal }}</div> --}}
-                            <div class="flex-col w-2/12 text-right">
+                            <div class="flex-col w-1/12 text-right">
                                 <select wire:change="changeValor({{ $presupuesto }},'estado',$event.target.value)"
                                     class="w-full text-center py-1 my-1 text-xs text-gray-600 placeholder-gray-300 bg-{{ $presupuesto->status_color[0] }} border-blue-300 rounded-md shadow-sm appearance-none hover:border-gray-400 focus:outline-none">
                                     <option value="0" {{ $presupuesto->estado== '0'? 'selected' : '' }}>Enviado</option>
@@ -69,11 +51,11 @@
                                     <a class="text-blue-700 underline " href="{{ route('pedido.editar',[$presupuesto->pedido ,'i']) }}"  title="Pedido">{{ $presupuesto->pedido }}</a>
                                 @endif
                             </div>
-                            <div class="flex flex-row-reverse w-1/12 ">
-                                <x-icon.delete-a wire:click.prevent="delete({{ $presupuesto->id }})" onclick="confirm('¿Estás seguro?') || event.stopImmediatePropagation()" class="pl-1"/>
+                            <div class="w-2/12 space-x-1 text-center lg:space-x-1">
                                 <x-icon.clip-a class="w-5 text-green-500 hover:text-green-700 " onclick="location.href = '{{route('presupuesto.archivos',[$presupuesto->id,'i'])}}'" title="Archivo"/>
-                                <a href="{{route('presupuesto.presupuestoPDF',$presupuesto)}}" target="_blank" ><x-icon.pdf class="text-red-500 hover:text-red-700 " title="PDF Presupuesto"/></a>
+                                <x-icon.pdf-a class=" text-red-500 hover:text-red-700" href="{{route('presupuesto.presupuestoPDF',$presupuesto)}}" target="_blank" title="PDF Presupuesto"/>
                                 <x-icon.edit-a class="" href="{{ route('presupuesto.editar',[$presupuesto,'i']) }}"  title="Editar"/>
+                                <x-icon.delete-a class="" wire:click.prevent="delete({{ $presupuesto->id }})" onclick="confirm('¿Estás seguro?') || event.stopImmediatePropagation()"/>
                             </div>
                         </div>
                         @empty
