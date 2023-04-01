4c071036a3a863b6223ed091b8193d9a5513d71e
Alexander Arregui <mosketxu@gmail.com>
2023-04-01 18:53:22 +0200

    pointer

--- resources/views/livewire/entidad/ents.blade.php
+++ resources/views/livewire/entidad/ents.blade.php
@@ -1,58 +1,52 @@
 <div class="">
-    {{-- @livewire('menu') --}}
     <div class="h-full p-1 mx-2">
         <h1 class="text-2xl font-semibold text-gray-900">{{ $entidadtipo->nombreplural ?? 'Contactos' }}
         <div class="py-1 space-y-4">
-            @if (session()->has('message'))
-                <div id="alert" class="relative px-6 py-2 mb-2 text-white bg-red-200 border-red-500 rounded border-1">
-                    <span class="inline-block mx-8 align-middle">
-                        {{ session('message') }}
-                    </span>
-                    <button class="absolute top-0 right-0 mt-2 mr-6 text-2xl font-semibold leading-none bg-transparent outline-none focus:outline-none" onclick="document.getElementById('alert').remove();">
-                        <span>×</span>
-                    </button>
-                </div>
-            @endif
-            <x-jet-validation-errors></x-jet-validation-errors>
+            <div class="">
+                @include('errores')
+            </div>
             <div class="">
                 @include('entidad.entidadfilters')
             </div>
-            {{-- tabla entidades --}}
             <div class="flex-col space-y-4">
                 <div>
-                    <div class="flex py-2 pl-2 text-sm text-left text-gray-500 bg-blue-100 rounded-t-md">
-                        <div class="flex w-3/12 font-light " >{{ $entidadtipo->nombre }}</div>
-                        <div class="hidden w-1/12 font-light lg:flex " >{{ __('Tipo') }}</div>
-                        <div class="hidden w-2/12 font-light md:w-2/12" >{{ __('Responsable') }}</div>
-                        <div class="hidden w-1/12 font-light md:flex" >{{ __('Nif') }} </div>
-                        <div class="hidden w-2/12 font-light lg:flex" >{{ __('Tfno.') }}</div>
-                        <div class="flex w-2/12 font-light lg:w-3/12" >{{ __('Email') }}</div>
-                        <div class="flex w-1/12" ></div>
+                    <div class="flex w-full py-2 text-sm text-left text-gray-500 bg-blue-100 rounded-t-md">
+                        <div class="flex w-11/12 ">
+                            <div class="w-3/12 pl-2 " >{{ $entidadtipo->nombre }}</div>
+                            <div class="w-1/12 ">{{ __('Tipo') }}</div>
+                            <div class="w-2/12 " >{{ __('Respble.') }}</div>
+                            <div class="w-1/12 " >{{ __('Nif') }} </div>
+                            <div class="w-2/12 " >{{ __('Tfno.') }}</div>
+                            <div class="w-3/12 " >{{ __('Email') }}</div>
+                        </div>
+                        <div class="flex w-1/12 ">
+                        </div>
                     </div>
                     <div>
                         @forelse ($entidades as $entidad)
-                            <div class="flex w-full py-1 text-sm text-left border-t-0 space-c-1 border-y hover:bg-gray-100" wire:loading.class.delay="opacity-50">
-                                <div class="flex w-3/12 ">
-                                    <input type="text" class="w-full p-1 text-sm font-thin text-gray-500 border-0 rounded-md hover:border-blue-500 focus:border-blue-500 " value="{{ $entidad->entidad }}"  readonly/>
+                            <div class="flex items-center w-full text-sm cursor-pointer  text-gray-500 border-t-0 border-y hover:bg-gray-100 hover:cursor-pointer" wire:loading.class.delay="opacity-50" >
+                                <div class="flex items-center w-11/12 my-1 cursor-pointer"  onclick="location.href = '{{ route('entidad.edit',$entidad) }}'">
+                                    <div class="w-3/12 pl-2  ">
+                                        <input type="text" class="w-full p-1 cursor-pointer text-sm font-thin text-gray-500 border-0 rounded-md hover:border-blue-500 focus:border-blue-500 " value="{{ $entidad->entidad }}"  disabled/>
+                                    </div>
+                                    <div class="w-1/12 ">
+                                        <input type="text" class="w-full p-1 cursor-pointer text-sm font-thin text-gray-500 border-0 rounded-md hover:bg-gray-100" value="{{ $entidad->entidadtipo->nombrecorto ?? '-'}}"  disabled/>
+                                    </div>
+                                    <div class="w-2/12 ">
+                                        <input type="text" class="w-full p-1 cursor-pointer text-sm font-thin text-gray-500 border-0 rounded-md hover:bg-gray-100" value="{{ $entidad->responsable}}"  disabled/>
+                                    </div>
+                                    <div class="w-1/12 ">
+                                        <input type="text" class="w-full p-1 cursor-pointer text-sm font-thin text-gray-500 border-0 rounded-md hover:bg-gray-100" value="{{ $entidad->nif }}" disabled/>
+                                    </div>
+                                    <div class="w-2/12 ">
+                                        <input type="text" class="w-full p-1 cursor-pointer text-sm font-thin text-gray-500 border-0 rounded-md hover:bg-gray-100" value="{{ $entidad->tfno }}" disabled/>
+                                    </div>
+                                    <div class="w-3/12 ">
+                                        <input type="text" class="w-full p-1 cursor-pointer text-sm font-thin text-gray-500 border-0 rounded-md hover:bg-gray-100" value="{{ $entidad->emailgral }}"  disabled/>
+                                    </div>
                                 </div>
-                                <div class="hidden w-1/12 lg:flex">
-                                    <input type="text" class="w-full p-1 text-sm font-thin text-gray-500 border-0 rounded-md hover:bg-gray-100" value="{{ $entidad->entidadtipo->nombrecorto ?? '-'}}"  readonly/>
-                                </div>
-                                <div class="hidden w-2/12 md:w-2/12">
-                                    <input type="text" class="w-full p-1 text-sm font-thin text-gray-500 border-0 rounded-md hover:bg-gray-100" value="{{ $entidad->responsable}}"  readonly/>
-                                </div>
-                                <div class="hidden w-1/12 md:flex">
-                                    <input type="text" class="w-full p-1 text-sm font-thin text-gray-500 border-0 rounded-md hover:bg-gray-100" value="{{ $entidad->nif }}" readonly/>
-                                </div>
-                                <div class="hidden w-2/12 lg:flex">
-                                    <input type="text" class="w-full p-1 text-sm font-thin text-gray-500 border-0 rounded-md hover:bg-gray-100" value="{{ $entidad->tfno }}" readonly/>
-                                </div>
-                                <div class="flex w-2/12 lg:w-3/12">
-                                    <input type="text" class="w-full p-1 text-sm font-thin text-gray-500 border-0 rounded-md hover:bg-gray-100" value="{{ $entidad->emailgral }}"  readonly/>
-                                </div>
-                                <div class="w-1/12 mx-auto space-x-2">
-                                    <x-icon.edit-a href="{{ route('entidad.edit',$entidad) }}"  title="Editar"/>
-                                    <x-icon.usergroup href="{{ route('entidad.contactos',$entidad) }}"  title="Contactos"/>
+                                <div class="items-center text-center w-1/12 " >
+                                    <x-icon.usergroup-a href="{{ route('entidad.contactos',$entidad) }}"  title="Contactos"/>
                                     <x-icon.plane-a class="text-gray-900 transform hover:text-black" href="{{ route('entidad.destinos',[$entidad,'i']) }}"  title="Destinos"/>
                                     <x-icon.delete-a wire:click.prevent="delete({{ $entidad->id }})" onclick="confirm('¿Estás seguro?') || event.stopImmediatePropagation()" class="w-6"/>
                                 </div>
@@ -71,9 +65,6 @@
                         @endforelse
                     </div>
                 </div>
-                {{-- <div>
-                    {{ $entidades->links() }}
-                </div> --}}
             </div>
         </div>
     </div>
