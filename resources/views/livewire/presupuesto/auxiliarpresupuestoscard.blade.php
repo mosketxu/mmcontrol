66af8dad345eef9236a4507a3837a2dc55f21c20
Alexander Arregui <mosketxu@gmail.com>
2023-04-30 11:07:12 +0200

    clip

--- resources/views/livewire/presupuesto/auxiliarpresupuestoscard.blade.php
+++ resources/views/livewire/presupuesto/auxiliarpresupuestoscard.blade.php
@@ -151,7 +151,7 @@
                                 <a href="{{asset('archivospresupuesto/'.$valor->valorcampoimg) }}" target="_blank"
                                     class="w-5 text-blue-500 hover:text-blue-700 hover:bg-gray-100" title="Ver producto">
                                     <div class="flex">
-                                        <x-icon.clip class="w-4"/>
+                                        <x-icon.clip class="w-8"/>
                                         <div class="mt-1">
                                             {{ $valor->valorcampo3 }}
                                         </div>
