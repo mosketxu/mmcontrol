b3d614b591e045ef6d5153de012f7489afcf80f3
Alexander Arregui <mosketxu@gmail.com>
2024-04-25 10:21:56 +0200

    natureflex

--- resources/views/pedidos/fichaentradaeditorialpdf.blade.php
+++ resources/views/pedidos/fichaentradaeditorialpdf.blade.php
@@ -80,8 +80,8 @@
                             <td  width=75% style="padding-left:10px;background-color: white;border-style: solid; border-width: .6; border-color: gray" colspan="2">{{ $pedido->pruebacolor }}</td>
                         </tr>
                         <tr style="">
-                            <td  width=25% style="font-weight:bold; padding-left:3px;background-color: #C2D69B;border-style: solid; border-width: .6; border-color: gray">Laminado Plástico</td>
-                            <td  width=75% style="padding-left:10px;background-color: white;border-style: solid; border-width: .6; border-color: gray" colspan="2">{{ $pedido->laminadoplastico == '1' ? 'Natureflex' : 'No'  }}</td>
+                            <td  width=25% style="font-weight:bold; padding-left:3px;background-color: #C2D69B;border-style: solid; border-width: .6; border-color: gray">Natureflex</td>
+                            <td  width=75% style="padding-left:10px;background-color: white;border-style: solid; border-width: .6; border-color: gray" colspan="2">{{ $pedido->laminadoplastico == '1' ? 'Sí' : 'No'  }}</td>
                         </tr>
                         @foreach ($pedido->distribuciones as $distribucion )
                         <tr>
