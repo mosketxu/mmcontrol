15376eed5b496d9de6fb0b585aa61c9022c2431d
Alexander Arregui <mosketxu@gmail.com>
2026-08-25 11:03:25 +0200

    Fix: usar alias correcto seguridad.usuarios en users/index.blade.php

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,7 +7,7 @@
                     <div class="p-1 mx-2">
                         <h1 class="text-2xl font-semibold text-gray-900">Usuarios y Seguridad</h1>
                         <div class="flex flex-row flex-wrap -mx-2">
-                            @livewire('usuarios')
+                            @livewire('seguridad.usuarios')
                         </div>
                     </div>
                 </div>
